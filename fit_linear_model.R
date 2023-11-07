f854e309ff26608c471ed3bba017b862b98b732e
Eve Taylor <eve.taylor@magd.ox.ac.uk>
2023-11-07 14:21:15 +0000

    awesomeupdate

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -2,18 +2,19 @@
 
 #library(dplyr)
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("/cloud/project/experiment1.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<???) %>% mutate(N_log = log(N))
-
+data_subset1 <- growth_data %>% filter(t<1500) %>% mutate(N_log = log(N))
+data_subset1
+#subset data to exponential stage, and carrying capacity stage, decided arbritrarily from the graph
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>???)
+data_subset2 <- growth_data %>% filter(t>2500)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -1,6 +1,6 @@
 #Script to plot data and model
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("/cloud/project/experiment1.csv")
 
 logistic_fun <- function(t) {
   
@@ -10,13 +10,13 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- ??? #
+N0 <- exp(6.8941709) #intercept of the 1st regression e^N0
   
-r <- ??? #
+r <- 0.0100086 #gradient of the 1st regression
   
-K <- ??? #
+K <- 6.00e+10 #intercept of 2nd regression
 
-ggplot(aes(???,???), data = growth_data) +
+ggplot(aes(t,N), data = growth_data) +
   
   geom_function(fun=logistic_fun, colour="red") +
   
@@ -25,3 +25,15 @@ ggplot(aes(???,???), data = growth_data) +
   #scale_y_continuous(trans='log10')
 
 
+ggplot(aes(t,N), data = growth_data) +
+  
+  geom_function(fun=logistic_fun, colour="red") +
+  
+  geom_point() +
+
+scale_y_continuous(trans='log10')
+
+#packages used
+sink(file = "package-versions.txt")
+sessionInfo()
+sink()
